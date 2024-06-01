630bed101e2a78dcd74309ca61b40e051f929030
Yusei Moriwaka <senruo0827@gmail.com>
2024-06-01 18:09:32 +0900

    add feature about RSS fetch.

--- app/internal/adapters/controllers/discord_controller.go
+++ app/internal/adapters/controllers/discord_controller.go
@@ -2,8 +2,10 @@ package controllers
 
 import (
 	"app/internal/utils"
+	"fmt"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/mmcdole/gofeed"
 )
 
 type DiscordController struct {
@@ -21,6 +23,10 @@ func (c *DiscordController) RegisterCommands() {
 			Name:        "ping",
 			Description: "Replies with pong",
 		},
+		{
+			Name:        "abobon-articles",
+			Description: "Fetch the data from https://ktaroabobon.github.io/index.xml with RSS",
+		},
 	}
 
 	for _, cmd := range commands {
@@ -33,12 +39,11 @@ func (c *DiscordController) RegisterCommands() {
 
 func (c *DiscordController) HandleSlashCommands(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	if i.Type == discordgo.InteractionApplicationCommand {
-		// switch i.ApplicationCommandData().Name {
-		// case "ping":
-		// 	c.handlePingCommand(s, i)
-		// }
-		if i.ApplicationCommandData().Name == "ping" {
+		switch i.ApplicationCommandData().Name {
+		case "ping":
 			c.handlePingCommand(s, i)
+		case "abobon-articles":
+			c.handleArticleCommand(s, i)
 		}
 	}
 }
@@ -55,4 +60,53 @@ func (c *DiscordController) handlePingCommand(s *discordgo.Session, i *discordgo
 	if err != nil {
 		c.Logger.ErrorLogger.Printf("Error responding to ping command: %v", err)
 	}
+}
+
+func (c *DiscordController) handleArticleCommand(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	items, err := fetchRSS("https://ktaroabobon.github.io/index.xml")
+	if err != nil {
+		c.Logger.ErrorLogger.Printf("Error fetching RSS feed: %v", err)
+		response := &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Error fetching articles.",
+			},
+		}
+		err = s.InteractionRespond(i.Interaction, response)
+		if err != nil {
+			c.Logger.ErrorLogger.Printf("Error responding to error message of abobon-articles command: %v", err)
+		}
+		return
+	}
+
+	// 最新の5件の記事を表示
+	content := "Latest articles:\n"
+	for i, item := range items {
+		if i >= 5 {
+			break
+		}
+		content += fmt.Sprintf("[%d] %s\n", i+1, item.Title)
+	}
+
+	// TODO: レスポンス部分
+	response := &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: content,
+		},
+	}
+
+	err = s.InteractionRespond(i.Interaction, response)
+	if err != nil {
+		c.Logger.ErrorLogger.Printf("Error responding to abobon-articles command: %v", err)
+	}
+}
+
+func fetchRSS(url string) ([]*gofeed.Item, error) {
+	fp := gofeed.NewParser()
+	feed, err := fp.ParseURL(url)
+	if err != nil {
+		return nil, err
+	}
+	return feed.Items, nil
 }
